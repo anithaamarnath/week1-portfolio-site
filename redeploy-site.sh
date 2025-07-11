d43cabfffb0c969109d6ab2bebd8ef79c81e5a14
anithaamarnath <anithaamarnath04@gmail.com>
2025-07-11 14:19:14 +0000

    Updated redeploy serviced

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-echo "List all tmux session:"
-tmux ls 2>/dev/null || echo "No tmux sessions running"
-
-echo "Killing tmux session..."
-tmux ls 2>/dev/null | cut -d: -f1 | xargs -r -n1 tmux kill-session -t
-
 
 echo "Changing to the actual project folder"
 PROJECT_DIR=week1-portfolio-site
-SESSION_NAME=week1
+
 
 
 echo "Changing directory to project: $PROJECT_DIR"
@@ -28,9 +22,16 @@ source venv/bin/activate
 echo "Installing python dependencies"
 pip install -r requirements.txt
 
-echo "Starting new detached tmux session: $SESSION_NAME"
-tmux new-session -d -s "$SESSION_NAME"
-tmux send-keys -t "$SESSION_NAME" "source venv/bin/activate" C-m
-tmux send-keys -t "$SESSION_NAME" "flask run --host=0.0.0.0 --port=5000" C-m
+echo "Restating myportfolio systemd service"
+
+sudo systemctl daemon-reload
+sudo systemctl restart myportfolio.service
+
+echo "Checking status"
+
+sudo systemctl status myportfolio.service
+
+
+echo "Deployment done"
 
 
